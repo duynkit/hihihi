e06419b5caa8487fe3cc2f6b6929629d3b9d2611
duynkit <121235947+duynkit@users.noreply.github.com>
2023-03-08 18:44:10 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Nga';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng người thương';
+const contentCard = 'Duy chúc Nga có một ngày 8/3 thật xinh đẹp và hạnh phúc bên gia đình và người thương. 🥰🥰';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'nga.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
