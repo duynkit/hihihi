f3cf9109c4cff8b718321b8e15789be570fb63d3
duynkit <121235947+duynkit@users.noreply.github.com>
2024-01-30 15:46:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-const nameGirl = 'Nga';
+const nameGirl = 'Mai đẹt ti ni';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 8-3';
-const titleCard = 'Tặng người thương';
-const contentCard = 'Duy chúc Nga có một ngày 8/3 thật xinh đẹp và hạnh phúc bên gia đình và người thương. 🥰🥰';
+const eventName = 'Chúc Mừng 30/01';
+const titleCard = 'Tặng em';
+const contentCard = 'Thật ra chả có cái gì đâu';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'nga.png';
+const giftImage = 'hihi.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
